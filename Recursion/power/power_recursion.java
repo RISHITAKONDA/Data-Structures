ee92585a4d0f5f9dc8f2e407b17154cb8f102737
Rishita Konda <rishitakonda@Rishitas-MacBook-Pro.attlocal.net>
2025-04-20 22:42:52 -0700

    Computed power using recursion - TC: O(logN)

--- Recursion/power/power_recursion.java
+++ Recursion/power/power_recursion.java
@@ -1,32 +1,25 @@
-package Recursion;
+//The time complexity is O(log N)
+//The space complexity is O(log n) because of the recursive call stack depth.
+
+
+package Recursion.power;
 
 class Solution {
     private double power(double x, long n) {
-        // Base case: anything raised to 0 is 1
-        if (n == 0) return 1.0;
-        
-        // Base case: anything raised to 1 is itself
-        if (n == 1) return x;
-        
-        // If 'n' is even
-        if (n % 2 == 0) {
-            // Recursive call: x * x, n / 2
+        if (n == 0) return 1.0; // when power is 0
+        if (n == 1) return x;   // when power is 1
+
+        if (n % 2 == 0) {   //power is even
             return power(x * x, n / 2);
         }
-        
-        // If 'n' is odd
-        // Recursive call: x * power(x, n - 1)
-        return x * power(x, n - 1);
+        return x * power(x, n - 1); // when power is odd
     }
     
     public double myPow(double x, int n) {
-        // If 'n' is negative
+
         if (n < 0) {
-            // Calculate the power of -n and take reciprocal
             return 1.0 / power(x, -n);
         }
-        
-        // If 'n' is non-negative
         return power(x, n);
     }
 
